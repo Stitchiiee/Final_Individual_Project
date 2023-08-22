6df76170381116dcdb594eabb2e928297141bff1
Stitchiiee <is328@njit.edu>
2023-08-22 15:55:04 -0400

    homepageupdate1.01

--- index/app/page.tsx
+++ index/app/page.tsx
@@ -9,54 +9,69 @@ function HomePage() {
       <nav>
       <ul className="navbar">
         <li className="nav-item">
-          <Link href="/">
+          <a href="/">
             Home
-          </Link>
+          </a>
         </li>
         <li className="nav-item">
-          <Link href="/about">
+          <a href="/about">
             About Me
-          </Link>
+          </a>
         </li>
         <li className="nav-item">
-          <Link href="/projects">
+          <a href="/projects">
             Projects
-          </Link>
+          </a>
         </li>
         <li className="nav-item">
-          <Link href="/contact">
+          <a href="/contact">
             Contact
-          </Link>
+          </a>
         </li>
         <li className="nav-item">
-          <Link href="/resume">
+          <a href="/resume">
             Resume
-          </Link>
+          </a>
         </li>
         <li className="nav-item">
-          <Link href="/privacypolicy">
+          <a href="/privacypolicy">
             Privacy Policy
-          </Link>
+          </a>
         </li>
         <li className="nav-item">
-          <Link href="/github">
+          <a href="/github">
             Github
-          </Link>
+          </a>
         </li>
         <li className="nav-item">
-          <Link href="/linkedin">
+          <a href="/linkedin">
             Linkedin
-          </Link>
+          </a>
         </li>
       </ul>
     </nav>
+    <main>
     <div className="hero">
       <h1>Patientia Est Virtus</h1>
-      <p>This is the home page of my awesome website built with Next.js!</p>
+      <p>This website was made to briefly talk about different aspects of my life. In particular my credentials, showcase projects I have made, my resume, my skills, and other provide ways to contact me.</p>
     </div>
-    </div>
-
+    <div className="briefbio">
+  <h3>Brief Bio</h3>
+  <p>My name is Ian Santucci and I am a Business and Information Systems major at NJIT. Last spring semester I graduated Sussex County Community College and aquired my associates degree in Information Systems. 
+    I am very excited to be graduating with my bacheolrs at the end of this spring. I want nothing more then to start working full time. Hopefully I can attain an intership or a COOP this year to get me more comfortable in my field.</p>
+</div>
+<div className="calltoaction">
+  <h3>Call To Action</h3>
+  <p>I would like you to further explore my website. The easiest way to travel through my websites pages is through the navigation bar at the top of the page. Feel free to look around!</p>
+</div>
+<div className="mailchimp">
+  <h3>Mailchimp</h3>
+  <p></p>
+</div>
+</main>
+<footer>
+</footer>
+</div>
   );
-}
-
-export default HomePage;
+};
+export default HomePage
