7e6030527c0f65ebd5673db5b1ac30776408c09b
Stitchiiee <is328@njit.edu>
2023-09-04 14:33:17 -0400

    changes

--- index/app/page.tsx
+++ index/app/page.tsx
@@ -37,12 +37,12 @@ function HomePage() {
           </a>
         </li>
         <li className="nav-item">
-          <a href="/github">
+          <a href="https://github.com/Stitchiiee">
             Github
           </a>
         </li>
         <li className="nav-item">
-          <a href="/linkedin">
+          <a href="https://www.linkedin.com/in/ian-santucci-8a135727a/">
             Linkedin
           </a>
         </li>
@@ -51,65 +51,65 @@ function HomePage() {
     <main>
     <div className="hero">
       <h1>Patientia Est Virtus</h1>
-      <p>Welcome to my portfolio website! I made this website to briefly talk about different aspects of my life. In particular my credentials, showcase projects I have made, my resume, my skills, and provide other ways to contact me.</p>
+      <p>Welcome to my portfolio website! I made this website to briefly talk about different aspects of my life. In particular my credentials, I will be showcasing projects I have made, my skills, and provide other ways to contact me.</p>
     </div>
     <div className="calltoaction">
   <h3>Call To Action</h3>
-  <p>I would like you to further explore my website. The easiest way to travel through my websites pages is through the navigation bar at the top of the page. Feel free to look around!</p>
+  <p>I would like you to further explore my website. The easiest way to travel through my websites page is through the navigation bar at the top of the page or just by scrolling through, Feel free to look around!</p>
 </div>
   <div id="aboutme">
   <h3>About Me</h3>
   <p>My name is Ian Santucci and I am a Business and Information Systems major at NJIT. Last spring semester I graduated Sussex County Community College and aquired my associates degree in Information Systems. 
-    I am very excited to be graduating with my bacheolrs at the end of this spring. I want nothing more then to start working full time. Hopefully I can attain an intership or a COOP this year to get me more comfortable in my field.</p>
+    I am very excited to be graduating with my bacheolrs in the spring of 2024. I want nothing more then to start working full time. Hopefully I can attain an intership or a COOP this year to get me more comfortable in my field.</p>
 </div>
 <div id="projects">
   <h3>Projects</h3>
-  <p>My name is Ian Santucci and I am a Business and Information Systems major at NJIT. Last spring semester I graduated Sussex County Community College and aquired my associates degree in Information Systems. 
-    I am very excited to be graduating with my bacheolrs at the end of this spring. I want nothing more then to start working full time. Hopefully I can attain an intership or a COOP this year to get me more comfortable in my field.</p>
+  <p>Some of the projects I have made include a text based adventure game written in Java, I helped organize my school yearbooks in highschool using photoshop, I edited tracks for a live dance recital using FL Studio, and I have made two other websites in basic HTML.  </p>
+<script></script>
 </div>
 <div id="privacypolicy">
   <h3>Privacy Policy</h3>
   <p>
-Last Updated: [8/22/2023]
+Last Updated: [8/22/2023]</p>
 
-Thank you for visiting my website. This Privacy Policy explains how I collect and use data on my website.
+<p>Thank you for visiting my website. This Privacy Policy explains how I collect and use data on my website.</p>
 
-Information I Collect:
+<p>Information I Collect:</p>
 
-I do not collect any personal information from my website visitors unless explicitly provided by you. However, I do collect and store the following non-personal information for statistical purposes:
+<p>I do not collect any personal information from my website visitors unless explicitly provided by you. However, I do collect and store the following non-personal information for statistical purposes:</p>
 
-Total number of visitors
-Page views
-Browser type and version
-Operating system
-Date and time of access
-Use of Information
+<p>Total number of visitors</p>
+<p>Page views</p>
+<p>Browser type and version</p>
+<p>Operating system</p>
+<p>Date and time of access</p>
+<p>Use of Information</p>
 
-The non-personal information I collect is used solely for the purpose of understanding how you interact with my website. This data helps me improve the content and functionality of my site and does not personally identify you.
+<p>The non-personal information I collect is used solely for the purpose of understanding how you interact with my website. This data helps me improve the content and functionality of my site and does not personally identify you.</p>
 
-Third-Party Services:
+<p>Third-Party Services:</p>
 
-I do not use third-party analytics services or tracking tools on my website.
+<p>I do not use third-party analytics services or tracking tools on my website.</p>
 
-Cookies:
+<p>Cookies:</p>
 
-I do not use cookies to collect or store personal information.
+<p>I do not use cookies to collect or store personal information.</p>
 
-Your Choices:
+<p>Your Choices:</p>
 
-You have the option to disable cookies in your web browser, although this may affect your experience on my website.
+<p>You have the option to disable cookies in your web browser, although this may affect your experience on my website.</p>
 
-Children's Privacy:
+<p>Children's Privacy:</p>
 
-My website is not directed at children under the age of 13, and I do not knowingly collect any personal information from children.
+<p>My website is not directed at children under the age of 13, and I do not knowingly collect any personal information from children.</p>
 
-Changes to this Privacy Policy:
+<p>Changes to this Privacy Policy:</p>
 
-I reserve the right to update or change this Privacy Policy at any time. Any changes will be effective immediately upon posting the updated Privacy Policy on my website.
+<p>I reserve the right to update or change this Privacy Policy at any time. Any changes will be effective immediately upon posting the updated Privacy Policy on my website.</p>
 
-Contact Me:
+<p>Contact Me:</p>
 
-If you have any questions or concerns regarding this Privacy Policy, please contact me at [Your Contact Information].</p>
+<p>If you have any questions or concerns regarding this Privacy Policy, please contact me at [Your Contact Information].</p>
 </div>
 <div id="contactme">
   <h3>Mailchimp</h3>
